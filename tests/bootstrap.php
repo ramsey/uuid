4fa3a7abfd88045385a6dd60fd4741e4aba1ec98
Ben Ramsey <ben@benramsey.com>
2014-02-19 19:41:12 -0600

    Moving moontoast/math and symfony/console to require-dev; fixes #20

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL | E_STRICT);
 
 // Ensure that composer has installed all dependencies
-if (!file_exists(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'composer.lock')) {
+if (!file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
     die("Dependencies must be installed using composer:\n\nphp composer.phar install --dev\n\n"
         . "See http://getcomposer.org for help with installing composer\n");
 }
