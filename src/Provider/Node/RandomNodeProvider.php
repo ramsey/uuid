e006f669adbf8f1dbf4ad198d0748753e95f5be9
Ben Ramsey <ben@benramsey.com>
2015-07-15 13:59:52 -0500

    Adding common docblock file header

--- src/Provider/Node/RandomNodeProvider.php
+++ src/Provider/Node/RandomNodeProvider.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * This file is part of the ramsey/uuid library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) Ben Ramsey <ben@benramsey.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @link https://benramsey.com/projects/ramsey-uuid/ Documentation
+ * @link https://packagist.org/packages/ramsey/uuid Packagist
+ * @link https://github.com/ramsey/uuid GitHub
+ */
 
 namespace Ramsey\Uuid\Provider\Node;
 
