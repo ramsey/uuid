61bded7d74e4a0a99f03585c764613f28d5da5f6
Jessica Mauerhan <jmauerhan@grovo.com>
2016-03-22 16:50:36 -0400

    Skipping AspectMock tests on HHVM.

--- tests/src/TestCase.php
+++ tests/src/TestCase.php
@@ -37,7 +37,7 @@ class TestCase extends \PHPUnit_Framework_TestCase
 
     protected function skipIfBigEndianHost()
     {
-        if (! self::isLittleEndianSystem()) {
+        if (!self::isLittleEndianSystem()) {
             $this->markTestSkipped(
                 'Skipping test targeting little-endian architectures.'
             );
@@ -47,5 +47,12 @@ class TestCase extends \PHPUnit_Framework_TestCase
     public static function isLittleEndianSystem()
     {
         return current(unpack('v', pack('S', 0x00FF))) === 0x00FF;
+    }
+
+    protected function skipIfHhvm()
+    {
+        if (getenv('PHP_VERSION') === 'hhvm') {
+            $this->markTestSkipped('Skipping test that cannot run on HHVM');
+        }
     }
 }
