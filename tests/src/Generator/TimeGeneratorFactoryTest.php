ad468a8e1468dae23ec01603ff957fa7a8c0e802
Jessica Mauerhan <jessicamauerhan@gmail.com>
2016-03-23 09:55:15 -0400

    factory test

---
+++ tests/src/Generator/TimeGeneratorFactoryTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Ramsey\Uuid\Test\Generator;
+
+use Ramsey\Uuid\Generator\TimeGeneratorFactory;
+use Ramsey\Uuid\Test\TestCase;
+
+class TimeGeneratorFactoryTest extends TestCase
+{
+    public function testGeneratorReturnsNewGenerator()
+    {
+        $timeProvider = $this->getMock('Ramsey\Uuid\Provider\TimeProviderInterface');
+        $nodeProvider = $this->getMock('Ramsey\Uuid\Provider\NodeProviderInterface');
+        $timeConverter = $this->getMock('Ramsey\Uuid\Converter\TimeConverterInterface');
+
+        $factory = new TimeGeneratorFactory($nodeProvider, $timeConverter, $timeProvider);
+        $generator = $factory->getGenerator();
+        $this->assertInstanceOf('Ramsey\Uuid\Generator\TimeGeneratorInterface', $generator);
+    }
+
+}
