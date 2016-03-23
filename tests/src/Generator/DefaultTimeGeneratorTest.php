ce30f4d87a3b8c9da61e759e6e803f0eeaf05e42
Jessica Mauerhan <jmauerhan@grovo.com>
2016-03-23 07:31:26 -0400

    test stubs for DefaultTimeGenerator

---
+++ tests/src/Generator/DefaultTimeGeneratorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Ramsey\Uuid\Test\Generator;
+
+use Ramsey\Uuid\Test\TestCase;
+
+class DefaultTimeGeneratorTest extends TestCase
+{
+    public function testGenerateUsesTimeProvidersCurrentTime()
+    {
+    }
+
+    public function testGenerateCalculatesTimeWithConverter()
+    {
+    }
+
+    public function testGenerateAppliesVersionToTime()
+    {
+    }
+
+    public function testGenerateAppliesVariantToClockSequence()
+    {
+    }
+
+    public function testGenerateReturnsBinaryStringInUuidFormat()
+    {
+    }
+
+    public function testGenerateUsesNodeProviderWhenNodeIsNull()
+    {
+    }
+
+    public function testGenerateUsesRandomSequenceWhenClockSeqNull()
+    {
+    }
+
+}
