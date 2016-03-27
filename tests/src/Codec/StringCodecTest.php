b3b6576057a5781fedb7a8f9c198c1813681c58a
Jessica Mauerhan <jessicamauerhan@gmail.com>
2016-03-27 10:29:04 -0400

    Test stubs for StringCodec

---
+++ tests/src/Codec/StringCodecTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Ramsey\Uuid\Test\Codec;
+
+use Ramsey\Uuid\Test\TestCase;
+
+/**
+ * Class StringCodecTest
+ * @package Ramsey\Uuid\Test\Codec
+ * @covers Ramsey\Uuid\Codec\StringCodec
+ */
+class StringCodecTest extends TestCase
+{
+    public function testEncodeUsesFieldsArray()
+    {
+
+    }
+
+    public function testEncodeReturnsFormattedString()
+    {
+
+    }
+
+    public function testEncodeBinaryUsesHexadecimalValue()
+    {
+
+    }
+
+    public function testEncodeBinaryReturnsBinaryString()
+    {
+
+    }
+
+    public function testDecodeUsesBulderOnFields()
+    {
+
+    }
+
+    public function testDecodeReturnsUuidFromBuilder()
+    {
+
+    }
+
+    public function testDecodeBytesThrowsExceptionWhenBytesStringNotSixteenCharacters()
+    {
+        
+    }
+
+    public function testDecodeBytesReturnsUuid()
+    {
+
+    }
+}
