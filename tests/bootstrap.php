f061b48e35a97363ce0f9e4febf326ec05f84e8e
Martin Hujer <mhujer@gmail.com>
2018-02-15 09:22:03 +0100

    Tests: configure cacheDir for AspectMock

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,5 +4,6 @@ include __DIR__ . '/../vendor/autoload.php'; // composer autoload
 $kernel = \AspectMock\Kernel::getInstance();
 $kernel->init([
     'debug' => true,
+    'cacheDir' => sys_get_temp_dir(),
     'includePaths' => [__DIR__ . '/../src']
 ]);
