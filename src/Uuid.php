39a6d5e6d315cdc9371fe4b87595f82580af0775
Michael Moravec <me@majkl.me>
2019-01-07 16:16:06 +0100

    Revert "Remove "final" keyword to allow extending"
    This reverts commit c7daee6c6a582110a1d60bb482a9644202d3c7a2.

--- src/Uuid.php
+++ src/Uuid.php
@@ -35,7 +35,7 @@ use Ramsey\Uuid\Exception\UnsupportedOperationException;
  * @link http://docs.python.org/3/library/uuid.html
  * @link http://docs.oracle.com/javase/6/docs/api/java/util/UUID.html
  */
-class Uuid implements UuidInterface
+final class Uuid implements UuidInterface
 {
     /**
      * When this namespace is specified, the name string is a fully-qualified domain name.
