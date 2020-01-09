deb888c4a35950c599ab2631e50c18e595573835
Ben Ramsey <ben@benramsey.com>
2020-01-09 16:59:04 -0600

    Set floating-point precision for tests to "14"

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,9 @@
 
 error_reporting(E_ALL & ~E_DEPRECATED);
 
+// Ensure floating-point precision is set to 14 (the default) for tests.
+ini_set('precision', '14');
+
 use AspectMock\Kernel;
 
 require_once __DIR__ . '/../vendor/autoload.php'; // composer autoload
