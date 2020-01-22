9988854cf1ab240a9596fa9747bd41aba571de03
Ben Ramsey <ben@benramsey.com>
2020-01-22 00:41:45 -0600

    Fix bug causing test to fail on 32-bit systems

--- src/Generator/DceSecurityGenerator.php
+++ src/Generator/DceSecurityGenerator.php
@@ -95,7 +95,12 @@ class DceSecurityGenerator implements DceSecurityGeneratorInterface
         }
 
         $domainByte = pack('n', $localDomain)[1];
-        $identifierBytes = pack('N*', $localIdentifier->toString());
+        $identifierBytes = hex2bin(str_pad(
+            $this->numberConverter->toHex($localIdentifier->toString()),
+            8,
+            '0',
+            STR_PAD_LEFT
+        ));
 
         if ($node instanceof Hexadecimal) {
             $node = $node->toString();
