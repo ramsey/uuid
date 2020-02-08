f965293da92ffdfee13f5f4f67f6803423bef17d
Ben Ramsey <ben@benramsey.com>
2020-02-08 13:13:47 -0600

    Specify cache locations for build tools

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,9 +16,17 @@ use AspectMock\Kernel;
 require_once __DIR__ . '/../vendor/autoload.php'; // composer autoload
 require_once __DIR__ . '/phpstan-bootstrap.php';
 
+$cacheDir = __DIR__ . '/../build/cache/goaop';
+if (!is_dir($cacheDir)) {
+    if (mkdir($cacheDir, 0775, true) === false) {
+        echo "\n[ERROR] Unable to create cache directory at {$cacheDir}\n\n";
+        exit(1);
+    }
+}
+
 $kernel = Kernel::getInstance();
 $kernel->init([
     'debug' => true,
-    'cacheDir' => sys_get_temp_dir(),
+    'cacheDir' => $cacheDir,
     'includePaths' => [__DIR__ . '/../src']
 ]);
