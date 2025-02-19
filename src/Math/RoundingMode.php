806f8209759acf52ace72fd32d0f534ea25e9af9
Kurund Jalmi <kurund.jalmi@gmail.com>
2025-02-19 12:34:02 +0000

    update RoundingMode to be compatiable with already upgraded brick/math

--- src/Math/RoundingMode.php
+++ src/Math/RoundingMode.php
@@ -31,116 +31,94 @@ declare(strict_types=1);
 namespace Ramsey\Uuid\Math;
 
 /**
- * Specifies a rounding behavior for numerical operations capable of discarding
- * precision.
+ * Specifies a rounding behavior for numerical operations capable of discarding precision.
  *
- * Each rounding mode indicates how the least significant returned digit of a
- * rounded result is to be calculated. If fewer digits are returned than the
- * digits needed to represent the exact numerical result, the discarded digits
- * will be referred to as the discarded fraction regardless the digits'
- * contribution to the value of the number. In other words, considered as a
- * numerical value, the discarded fraction could have an absolute value greater
- * than one.
+ * Each rounding mode indicates how the least significant returned digit of a rounded result
+ * is to be calculated. If fewer digits are returned than the digits needed to represent the
+ * exact numerical result, the discarded digits will be referred to as the discarded fraction
+ * regardless the digits' contribution to the value of the number. In other words, considered
+ * as a numerical value, the discarded fraction could have an absolute value greater than one.
  */
-final class RoundingMode
+enum RoundingMode
 {
     /**
-     * Private constructor. This class is not instantiable.
+     * Asserts that the requested operation has an exact result, hence no rounding is necessary.
      *
-     * @codeCoverageIgnore
+     * If this rounding mode is specified on an operation that yields a result that
+     * cannot be represented at the requested scale, a RoundingNecessaryException is thrown.
      */
-    private function __construct()
-    {
-    }
-
-    /**
-     * Asserts that the requested operation has an exact result, hence no
-     * rounding is necessary.
-     */
-    public const UNNECESSARY = 0;
+    case UNNECESSARY;
 
     /**
      * Rounds away from zero.
      *
      * Always increments the digit prior to a nonzero discarded fraction.
-     * Note that this rounding mode never decreases the magnitude of the
-     * calculated value.
+     * Note that this rounding mode never decreases the magnitude of the calculated value.
      */
-    public const UP = 1;
+    case UP;
 
     /**
      * Rounds towards zero.
      *
-     * Never increments the digit prior to a discarded fraction (i.e.,
-     * truncates). Note that this rounding mode never increases the magnitude of
-     * the calculated value.
+     * Never increments the digit prior to a discarded fraction (i.e., truncates).
+     * Note that this rounding mode never increases the magnitude of the calculated value.
      */
-    public const DOWN = 2;
+    case DOWN;
 
     /**
      * Rounds towards positive infinity.
      *
-     * If the result is positive, behaves as for UP; if negative, behaves as for
-     * DOWN. Note that this rounding mode never decreases the calculated value.
+     * If the result is positive, behaves as for UP; if negative, behaves as for DOWN.
+     * Note that this rounding mode never decreases the calculated value.
      */
-    public const CEILING = 3;
+    case CEILING;
 
     /**
      * Rounds towards negative infinity.
      *
-     * If the result is positive, behave as for DOWN; if negative, behave as for
-     * UP. Note that this rounding mode never increases the calculated value.
+     * If the result is positive, behave as for DOWN; if negative, behave as for UP.
+     * Note that this rounding mode never increases the calculated value.
      */
-    public const FLOOR = 4;
+    case FLOOR;
 
     /**
-     * Rounds towards "nearest neighbor" unless both neighbors are equidistant,
-     * in which case round up.
+     * Rounds towards "nearest neighbor" unless both neighbors are equidistant, in which case round up.
      *
-     * Behaves as for UP if the discarded fraction is >= 0.5; otherwise, behaves
-     * as for DOWN. Note that this is the rounding mode commonly taught at
-     * school.
+     * Behaves as for UP if the discarded fraction is >= 0.5; otherwise, behaves as for DOWN.
+     * Note that this is the rounding mode commonly taught at school.
      */
-    public const HALF_UP = 5;
+    case HALF_UP;
 
     /**
-     * Rounds towards "nearest neighbor" unless both neighbors are equidistant,
-     * in which case round down.
+     * Rounds towards "nearest neighbor" unless both neighbors are equidistant, in which case round down.
      *
-     * Behaves as for UP if the discarded fraction is > 0.5; otherwise, behaves
-     * as for DOWN.
+     * Behaves as for UP if the discarded fraction is > 0.5; otherwise, behaves as for DOWN.
      */
-    public const HALF_DOWN = 6;
+    case HALF_DOWN;
 
     /**
-     * Rounds towards "nearest neighbor" unless both neighbors are equidistant,
-     * in which case round towards positive infinity.
+     * Rounds towards "nearest neighbor" unless both neighbors are equidistant, in which case round towards positive infinity.
      *
-     * If the result is positive, behaves as for HALF_UP; if negative, behaves
-     * as for HALF_DOWN.
+     * If the result is positive, behaves as for HALF_UP; if negative, behaves as for HALF_DOWN.
      */
-    public const HALF_CEILING = 7;
+    case HALF_CEILING;
 
     /**
-     * Rounds towards "nearest neighbor" unless both neighbors are equidistant,
-     * in which case round towards negative infinity.
+     * Rounds towards "nearest neighbor" unless both neighbors are equidistant, in which case round towards negative infinity.
      *
-     * If the result is positive, behaves as for HALF_DOWN; if negative, behaves
-     * as for HALF_UP.
+     * If the result is positive, behaves as for HALF_DOWN; if negative, behaves as for HALF_UP.
      */
-    public const HALF_FLOOR = 8;
+    case HALF_FLOOR;
 
     /**
-     * Rounds towards the "nearest neighbor" unless both neighbors are
-     * equidistant, in which case rounds towards the even neighbor.
+     * Rounds towards the "nearest neighbor" unless both neighbors are equidistant, in which case rounds towards the even neighbor.
      *
-     * Behaves as for HALF_UP if the digit to the left of the discarded fraction
-     * is odd; behaves as for HALF_DOWN if it's even.
+     * Behaves as for HALF_UP if the digit to the left of the discarded fraction is odd;
+     * behaves as for HALF_DOWN if it's even.
      *
      * Note that this is the rounding mode that statistically minimizes
      * cumulative error when applied repeatedly over a sequence of calculations.
-     * It is sometimes known as "Banker's rounding", and is chiefly used in the
-     * USA.
+     * It is sometimes known as "Banker's rounding", and is chiefly used in the USA.
      */
-    public const HALF_EVEN = 9;
+    case HALF_EVEN;
 }
